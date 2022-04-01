e78007f508b17d27994c869a2e942c609741f16d
hugosouza77 <102319355+hugosouza77@users.noreply.github.com>
2022-04-01 20:32:45 -0300

    Add files via upload

---
+++ questão02_js/js/index.js
@@ -0,0 +1,19 @@
+     var bimestreum = 9;
+       var bimestredois = 6;
+       var bimestretres = 7;
+       var bimestrequatro = 5;
+       var totaladicao = bimestreum + bimestredois + bimestretres + bimestrequatro ;
+       var totaldivisao = totaladicao / 4 ;
+
+         console.log(totaladicao);
+         console.log(totaldivisao);
+         document.write(" a média final é : " + totaldivisao);
+
+    var nota = 6.0;
+      if(nota >= 6){
+         document.write(" <br> situação : o aluno foi aprovado ");
+    } else if((nota < 6) && (nota >= 6)) {
+         document.write(" <br> situação : o aluno está de recuperação ");
+    } else {
+         document.write(" <br> situação : o aluno foi reprovado ");
+ }
