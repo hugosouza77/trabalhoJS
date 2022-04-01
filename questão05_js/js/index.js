c14088e6410f4bc35e9bde9a2a4d070a889126c7
hugosouza77 <102319355+hugosouza77@users.noreply.github.com>
2022-04-01 20:57:17 -0300

    Add files via upload

---
+++ questão05_js/js/index.js
@@ -0,0 +1,14 @@
+let code = 1;
+   if ( code == 1) {
+       console.log(" alimentos não perecivel");
+   }else if (code >= 2 && code <=4) {
+       console.log (" alimentos perecivel");
+   }else if ( code == 5 || code ==6){
+       console.log("vestuário");
+   }else if (code ==7){
+       console.log(" higiene pessoal");
+   }else if (code >=8 && code <=15);{
+         console.log(" limpeza e utensilios domésticos");
+   } {
+        console.log(" valor inválido");
+   }
