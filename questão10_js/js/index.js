75ad7a74c27bec4630ceaec1abad97312d7f0e68
hugosouza77 <102319355+hugosouza77@users.noreply.github.com>
2022-04-01 22:03:42 -0300

    Add files via upload

---
+++ questão10_js/js/index.js
@@ -0,0 +1,8 @@
+for (let i = 1 ; i < 11; i++){
+    document.write("<br>" +i)
+}
+   for (let i = 10 ; i > 0; i --){
+       if( i === 5) {
+           continue;   }
+       document.write(" <br>" +i)
+   }
